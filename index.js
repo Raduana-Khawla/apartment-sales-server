ffdcc587ef5c6adcf8875c084e4025769df5e5c6
raduanakhawla <raduanakhawla468@gmail.com>
2021-11-15 13:26:51 +0600

    done

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
+
+const port = process.env.PORT || 8000;
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wjlgu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -12,14 +17,10 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-const port = process.env.PORT || 8000;
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json("Hello World!");
 });
 
 async function run() {
@@ -35,23 +36,22 @@ async function run() {
       const ordersCollection = client.db("apartmentSales").collection("orders");
       const reviewCollection = client.db("apartmentSales").collection("review");
 
-      console.log("hello");
       //add serviceCollection
       app.post("/addServices", async (req, res) => {
         console.log(req.body);
         const result = await serviceCollection.insertOne(req.body);
-        res.send(result);
+        res.json(result);
       });
 
       // get all services
       app.get("/allServices", async (req, res) => {
         const result = await serviceCollection.find({}).toArray();
-        res.send(result);
+        res.json(result);
       });
       // get all ServiceItems
       app.get("/allServiceItems", async (req, res) => {
         const result = await serviceItemsCollection.find({}).toArray();
-        res.send(result);
+        res.json(result);
       });
       // single service
       app.get("/singleService/:id", async (req, res) => {
@@ -59,63 +59,62 @@ async function run() {
         const result = await serviceCollection
           .find({ _id: ObjectId(req.params.id) })
           .toArray();
-        res.send(result[0]);
-        console.log(result);
+        res.json(result[0]);
       });
       // insert order and
 
       app.post("/addOrders", async (req, res) => {
+        // console.log(req.body);
         const result = await ordersCollection.insertOne(req.body);
-        res.send(result);
+        res.json(result);
       });
 
       //  my order
 
       app.get("/myOrder/:email", async (req, res) => {
-        console.log(req.params.email);
+        // console.log(req.params.email);
         const result = await ordersCollection
           .find({ email: req.params.email })
           .toArray();
-        res.send(result);
+        res.json(result);
       });
 
       //post review
       app.post("/addReview", async (req, res) => {
         const result = await reviewCollection.insertOne(req.body);
-        res.send(result);
+        res.json(result);
       });
       //get review
       app.get("/addReview", async (req, res) => {
         const result = await reviewCollection.find({}).toArray();
-        res.send(result);
+        res.json(result);
       });
 
       app.post("/addUserInfo", async (req, res) => {
         console.log("req.body");
         const result = await usersCollection.insertOne(req.body);
-        res.send(result);
-        console.log(result);
+        res.json(result);
       });
       //  make admin
 
-      app.put("/makeAdmin", async (req, res) => {
-        const filter = { email: req.body.email };
-        const result = await usersCollection.find(filter).toArray();
-        if (result) {
-          const documents = await usersCollection.updateOne(filter, {
-            $set: { role: "admin" },
-          });
-          console.log(documents);
-        }
-      });
+      // app.put("/makeAdmin", async (req, res) => {
+      //   const filter = { email: req.body.email };
+      //   const result = await usersCollection.find(filter).toArray();
+      //   if (result) {
+      //     const documents = await usersCollection.updateOne(filter, {
+      //       $set: { role: "admin" },
+      //     });
+      //     console.log(documents);
+      //   }
+      // });
 
       // check admin or not
       app.get("/checkAdmin/:email", async (req, res) => {
         const result = await usersCollection
           .find({ email: req.params.email })
           .toArray();
-        console.log(result);
-        res.send(result);
+
+        res.json(result);
       });
       //order delete
       app.delete("/deleteOrder/:id", async (req, res) => {
@@ -123,13 +122,13 @@ async function run() {
           _id: ObjectId(req.params.id),
         });
         // console.log(result);
-        res.send(result);
+        res.json(result);
       });
       /// all order
       app.get("/allOrders", async (req, res) => {
         // console.log("hello");
         const result = await ordersCollection.find({}).toArray();
-        res.send(result);
+        res.json(result);
       });
 
       // status update
@@ -141,8 +140,7 @@ async function run() {
             status: req.body.status,
           },
         });
-        res.send(result);
-        console.log(result);
+        res.json(result);
       });
     });
   } finally {
@@ -152,12 +150,6 @@ async function run() {
 
 run().catch(console.dir);
 
-app.get("/", (req, res) => {
-  res.send("Running Noor Construction");
+app.listen(port, () => {
+  console.log("hello");
 });
-
-app.listen(port, (err) => {
-  if (err) console.log("error, err");
-});
-
-// app.listen(process.env.PORT || port);
